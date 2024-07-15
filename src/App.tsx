b40391cb60667ce25f6ab122153b492c6c9254c5
Henry Nguyen <Henry.Nguyen@fcsamerica.com>
2024-07-15 17:00:24 -0500

    finished tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import ListGroup from "./components/ListGroup";
-function App(){
-  return <div><ListGroup/></div>
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+function App() {
+  const [alertVisble, setAlertVisblility] = useState(false);
+
+  return (
+    <div>
+      {alertVisble && (
+        <Alert onClose={() => setAlertVisblility(false)}>My Alert</Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisblility(true)}>
+        Wassuh
+      </Button>
+    </div>
+  );
 }
 
 export default App;
